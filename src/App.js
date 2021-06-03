2c87f7338f5c56439a17f62f274d538d39fefc74
Becks Yates <becksyates@MacBook-Air-de-Becks.local>
2021-06-03 16:31:53 +0200

    Installed axios, bootstrap and added HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="main-sizing">
+          <div className="weather-app">
+            <div className="weather-info">
+              <div className="row">
+                <div className="col-6">
+                  <h1 id="city">Barcelona</h1>
+                  <ul>
+                    <li>
+                      Last updated: <span id="date"></span>,{" "}
+                      <span id="description"></span>
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <strong>
+                        <span id="humidity">20</span>%
+                      </strong>
+                      , Wind:{" "}
+                      <strong>
+                        <span id="wind">3</span>km/h
+                      </strong>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6 current-temp">
+                  <img src="" alt="" id="icon" />
+                  <span id="temperature"></span>
+                  <small className="weather-unit">
+                    <a href="#" id="celcius-link" className="active">
+                      20°C
+                    </a>{" "}
+                    |
+                    <a href="#" id="farenheit-link">
+                      °F
+                    </a>
+                  </small>
+                </div>
+              </div>
+            </div>
+            <form id="search-form">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city..."
+                    className="form-control search-input"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    value="Search"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="weather-forecast" id="forecast"></div>
+          </div>
+          <small>
+            <a
+              href="https://github.com/Becks-395/vanilla-weather-app"
+              target="_blank"
+            >
+              Open source code
+            </a>
+            , by Becks Yates
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
