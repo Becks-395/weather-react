4efe76aa59cf041cdb90662add786bf18baddccc
Becks Yates <becksyates@macbook-air-de-becks.home>
2021-06-07 09:41:54 +0200

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
@@ -78,5 +78,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
