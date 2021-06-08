1fa4df0e8d2b3a239aa30c54c3bd502910d26c47
Becks Yates <becksyates@macbook-air-de-becks.home>
2021-06-08 10:05:28 +0200

    Updated file structure and CSS

--- src/App.js
+++ src/App.js
@@ -1,80 +1,16 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
+  <div className="App">
       <div className="container">
-        <div className="main-sizing">
-          <div className="weather-app">
-            <div className="weather-info">
-              <div className="row">
-                <div className="col-6">
-                  <h1 id="city">Barcelona</h1>
-                  <ul>
-                    <li>
-                      Last updated: <span id="date"></span>,{" "}
-                      <span id="description"></span>
-                    </li>
-                    <li>
-                      Humidity:{" "}
-                      <strong>
-                        <span id="humidity">20</span>%
-                      </strong>
-                      , Wind:{" "}
-                      <strong>
-                        <span id="wind">3</span>km/h
-                      </strong>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-6 current-temp">
-                  <img src="" alt="" id="icon" />
-                  <span id="temperature"></span>
-                  <small className="weather-unit">
-                    <a href="#" id="celcius-link" className="active">
-                      20°C
-                    </a>{" "}
-                    |
-                    <a href="#" id="farenheit-link">
-                      °F
-                    </a>
-                  </small>
-                </div>
-              </div>
-            </div>
-            <form id="search-form">
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    placeholder="Enter a city..."
-                    className="form-control search-input"
-                    id="city-input"
-                    autocomplete="off"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    className="btn btn-primary w-100"
-                    value="Search"
-                  />
-                </div>
-              </div>
-            </form>
-            <div className="weather-forecast" id="forecast"></div>
-          </div>
-          <small>
-            <a
-              href="https://github.com/Becks-395/weather-react"
-              target="_blank"
-            >
-              Open source code
-            </a>
-            , by Becks Yates
-          </small>
-        </div>
+    <Weather />  
       </div>
-    </div>
-  );
+    <footer>
+      <a href="https://github.com/Becks-395/weather-react" target="_blank">Open source code
+      </a>, by Becks Yates
+    </footer>
+  </div>
+  )
 }
---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+          <div className="Weather">
+            <div className="weather-info">
+              <div className="row">
+                <div className="col-6">
+                  <h1 id="city">Barcelona</h1>
+                  <ul>
+                    <li>
+                      Last updated: <span id="date"></span>,{" "}
+                      <span id="description"></span>
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <strong>
+                        <span id="humidity">20</span>%
+                      </strong>
+                      , Wind:{" "}
+                      <strong>
+                        <span id="wind">3</span>km/h
+                      </strong>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6 current-temp">
+                  <img src="" alt="" id="icon" />
+                  <span id="temperature"></span>
+                  <small className="weather-unit">
+                    <a href="#" id="celcius-link" className="active">
+                      20°C
+                    </a>{" "}
+                    |
+                    <a href="#" id="farenheit-link">
+                      °F
+                    </a>
+                  </small>
+                </div>
+              </div>
+            </div>
+            <form id="search-form">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city..."
+                    className="form-control search-input"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    value="Search"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="weather-forecast" id="forecast"></div>
+          </div>
+  );
+}
